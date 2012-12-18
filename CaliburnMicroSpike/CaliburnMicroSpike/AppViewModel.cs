412a0562777fc4d4bdeb9f676cf37e27032c4dda
Michael Lam <michael@mapletonhillmedia.com>
2012-12-18 15:18:03 -0700

    hooking up convention based data binding

--- CaliburnMicroSpike/CaliburnMicroSpike/AppViewModel.cs
+++ CaliburnMicroSpike/CaliburnMicroSpike/AppViewModel.cs
@@ -4,6 +4,27 @@ namespace CaliburnMicroSpike
 {
 	public class AppViewModel : PropertyChangedBase
 	{
-		 
+		private int _count = 50;
+
+		public int Count
+		{
+			get { return _count; }
+			set 
+			{ 
+				_count = value;
+				NotifyOfPropertyChange(() => Count);
+				NotifyOfPropertyChange(() => CanIncrementCount);
+			}
+		}
+
+		public void IncrementCount()
+		{
+			Count++;
+		}
+
+		public bool CanIncrementCount
+		{
+			get { return Count < 60; }
+		}
 	}
 }
